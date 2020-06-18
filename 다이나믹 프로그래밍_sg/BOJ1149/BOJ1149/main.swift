1de9c6f925b94fc6c654e548e8b97dca52253773
seging <lksgfj93@naver.com>
2020-06-18 20:37:53 +0900

    BOJ1149 sg

--- 다이나믹 프로그래밍_sg/BOJ1149/BOJ1149/main.swift
+++ 다이나믹 프로그래밍_sg/BOJ1149/BOJ1149/main.swift
@@ -1,21 +1,16 @@
-let n = Int(readLine()!)!
-var dp = [[Int]](repeating: Array(repeating: 0, count: 3), count: 1001)
-var cost = [[Int]](repeating: Array(repeating: 0, count: 3), count: n+1)
-for i in 1...n
-{
-    let line = readLine()!.split(separator: " ").map({Int($0)!})
-    cost[i][0] = line[0]
-    cost[i][1] = line[1]
-    cost[i][2] = line[2]
+readLine()
+var a=[0,0,0]
+while let l=readLine(){
+    var t=a
+    let b=l.split{$0==" "}.map{Int($0)!}
+    for (i,e) in b.enumerated(){
+        var c=[Int]()
+        for (j,f) in a.enumerated(){
+            if i==j{continue}
+            c.append(f)
+        }
+        t[i]=min(c[0], c[1])+e
+    }
+    a=t
 }
-
-for i in 1...n
-{
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2]
-    
-    
-}
-
-print(min(dp[n][0], min(dp[n][1], dp[n][2])))
+print(a.min()!)
