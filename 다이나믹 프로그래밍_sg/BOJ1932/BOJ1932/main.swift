08b1bf18c443b22d04fe334f28f645c18ef0594c
seging <lksgfj93@naver.com>
2020-06-24 20:14:25 +0900

    1932BOJ

--- 다이나믹 프로그래밍_sg/BOJ1932/BOJ1932/main.swift
+++ 다이나믹 프로그래밍_sg/BOJ1932/BOJ1932/main.swift
@@ -1,29 +1,24 @@
 let T = Int(readLine()!)!
-var dp = [[Int]](repeating: Array(repeating: 0, count: 0), count: T + 1)
-var cnt = 1
-for _ in 0..<T
+var dp = [[Int]](repeating: Array(repeating: 0, count: T + 1), count: T + 1)
+var arr = [[Int]](repeating: Array(repeating: 0, count: T + 1), count: T + 1)
+
+for i in 1...T
 {
     let n = readLine()
     let t = n!.split(separator: " ").map({Int($0)!})
-    if t.count > 2
+    for j in 1...i
     {
-        for i in 0..<t.count-1
-        {
-            dp[cnt].append(t[i] + dp[cnt - 1][i])
-            dp[cnt].append(t[i + 1] + dp[cnt - 1][i])
+        arr[i][j] = t[j-1]
+    }
+}
 
-
-        }
-    }
-    else if t.count == 1
+for i in 1...T
+{
+    for j in 1...i
     {
-        dp[cnt].append(t[0])
+        
+        dp[i][j] = arr[i][j] + max(dp[i-1][j], dp[i-1][j-1])
+        
     }
-    else if t.count == 2
-    {
-        dp[cnt].append(dp[cnt - 1][0] + t[0])
-        dp[cnt].append(dp[cnt - 1][0] + t[1])
-    }
-    cnt += 1
 }
 print(dp[T].max()!)
